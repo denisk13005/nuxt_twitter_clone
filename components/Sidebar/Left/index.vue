084c1feced578ca9f34e8e1f0341205cc71202ac
denisk13005 <kirmanndenis@gmail.com>
2022-09-04 22:02:47 +0200

    mise en place des icons

--- components/Sidebar/Left/index.vue
+++ components/Sidebar/Left/index.vue
@@ -14,6 +14,42 @@
       </template>
       <template v-slot:name> Home </template>
     </SidebarLeftTab>
+    <SidebarLeftTab>
+      <template v-slot:icon>
+        <img class="w-8 h-8" src="~/assets/tag_black_24dp.svg" alt="" />
+      </template>
+      <template v-slot:name> Explore </template>
+    </SidebarLeftTab>
+    <SidebarLeftTab>
+      <template v-slot:icon>
+        <span class="material-icons"> messages </span>
+      </template>
+      <template v-slot:name> Messages </template>
+    </SidebarLeftTab>
+    <SidebarLeftTab>
+      <template v-slot:icon>
+        <span class="material-icons"> bookmarks </span>
+      </template>
+      <template v-slot:name> Bookmarks </template>
+    </SidebarLeftTab>
+    <SidebarLeftTab>
+      <template v-slot:icon>
+        <span class="material-icons"> list </span>
+      </template>
+      <template v-slot:name> Lists </template>
+    </SidebarLeftTab>
+    <SidebarLeftTab>
+      <template v-slot:icon>
+        <span class="material-icons"> person </span>
+      </template>
+      <template v-slot:name> Profile </template>
+    </SidebarLeftTab>
+    <SidebarLeftTab>
+      <template v-slot:icon>
+        <span class="material-icons"> more </span>
+      </template>
+      <template v-slot:name> More </template>
+    </SidebarLeftTab>
   </div>
 </template>
 <script setup>
